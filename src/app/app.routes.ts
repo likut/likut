089f0d964c62f9498c6335439e434a4f73c912ce
Or Perry <orperry@gmail.com>
2026-09-01 06:08:58 +0300

    coming soon page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./components/coming-soon/coming-soon').then((c) => c.ComingSoon),
+  },
+];
---
+++ src/app/components/coming-soon/coming-soon.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-coming-soon',
+  styleUrl: './coming-soon.scss',
+  templateUrl: './coming-soon.html',
+})
+export class ComingSoon {}
